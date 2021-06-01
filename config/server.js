e4d0f72f50809935f07f0c9466409c70520823b0
kridt <chrnielsen2003@gmail.com>
2021-06-02 00:18:12 +0200

    done

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '61e0167debfca00eb72db48267f17e45'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2b614306-c5ba-4ab9-88ef-2aea41d70353'
+};
